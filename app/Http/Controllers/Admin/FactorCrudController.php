92a9ba950147876fe173235a590d9bd80517174e
MohammadAtefrad <mo.atefrad@gmail.com>
2018-12-26 09:19:05 +0330

    add factor crud in admin panel.

--- app/Http/Controllers/Admin/FactorCrudController.php
+++ app/Http/Controllers/Admin/FactorCrudController.php
@@ -24,7 +24,7 @@ class FactorCrudController extends CrudController
         */
         $this->crud->setModel('App\Factor');
         $this->crud->setRoute(config('backpack.base.route_prefix') . '/factor');
-        $this->crud->setEntityNameStrings('factor', 'factors');
+        $this->crud->setEntityNameStrings('فاکتور', 'فاکتورها');
 
         /*
         |--------------------------------------------------------------------------
@@ -33,7 +33,64 @@ class FactorCrudController extends CrudController
         */
 
         // TODO: remove setFromDb() and manually define Fields and Columns
-        $this->crud->setFromDb();
+        // $this->crud->setFromDb();
+
+        // ------ CRUD FIELDS
+        $this->crud->addField([
+            'name' => 'user_id',
+            'type' => 'select2',
+            'label' => 'نام کاربر',
+            'entity' => 'user',
+            'attribute' => 'name',
+        ]);
+        $this->crud->addField([
+            'name' => 'books',
+            'type' => 'select2_multiple',
+            'label' => "کتابها",
+            'entity' => 'books',
+            'attribute' => 'name',
+            'pivot' => true,
+            'select_all' => true,
+
+        ]);
+        $this->crud->addField([
+            'name' => 'factor_status_id',
+            'type' => 'select2',
+            'label' => 'وضعیت فاکتور',
+            'entity' => 'factorStatus',
+            'attribute' => 'factor_status',
+        ]);
+
+        // ------ CRUD COLUMNS
+        $this->crud->addColumn([
+            'name' => 'id',
+            'type' => 'number',
+            'label' => 'شناسه',
+        ]);
+        $this->crud->addColumn([
+            'name' => 'user_id',
+            'type' => 'select',
+            'label' => 'نام کاربر',
+            'entity' => 'user',
+            'attribute' => 'name',
+        ]);
+        $this->crud->addColumn([
+            'name' => 'books_number',
+            'type' => 'select_multiple',
+            'label' => "کتابها",
+            'entity' => 'books',
+            'attribute' => 'name',
+        ]);
+        $this->crud->addColumn([
+            'name' => 'created_at',
+            'type' => 'datetime',
+            'label' => 'زمان ایجاد',
+        ]);
+        $this->crud->addColumn([
+            'name' => 'updated_at',
+            'type' => 'datetime',
+            'label' => 'زمان آخرین تغییرات',
+        ]);
 
         // add asterisk for fields that are required in FactorRequest
         $this->crud->setRequiredFields(StoreRequest::class, 'create');
@@ -43,6 +100,8 @@ class FactorCrudController extends CrudController
     public function store(StoreRequest $request)
     {
         // your additional operations before save here
+        $request->request->add(['books_number' => count($request->books)]);
+
         $redirect_location = parent::storeCrud($request);
         // your additional operations after save here
         // use $this->data['entry'] or $this->crud->entry
@@ -52,6 +111,8 @@ class FactorCrudController extends CrudController
     public function update(UpdateRequest $request)
     {
         // your additional operations before save here
+        $request->request->add(['books_number' => count($request->books)]);
+
         $redirect_location = parent::updateCrud($request);
         // your additional operations after save here
         // use $this->data['entry'] or $this->crud->entry
